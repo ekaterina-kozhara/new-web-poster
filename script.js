9074ccb3c337777e08a8b35b2c8815ed2b231f69
Ekaterina Kozhara <ekaterina.kozhara@yandex.ru>
2019-03-17 20:51:41 +0300

    анимировала первую заставку

---
+++ script.js
@@ -0,0 +1,34 @@
+	// $(function(){
+	// 			$("#blinkingCircle").hide();
+	// 			setTimeout(function(){$("#blinkingCircle").show();setInterval(function(){$("#blinkingCircle").toggle();},400)},1000);
+	// 		});
+	//
+	// setTimeout(function(){
+	//     $( ".first" ).css( {"display": "none" } )
+	// 		$( ".first" ).css( {"transition": "3s"} )
+	// }, 3000);
+	//
+	// setTimeout(function(){
+	// 		$( ".background" ).css( {"background-color": "#FFE3E3" } )
+	// 	  $( ".background" ).css( {"transition": "3s"} )
+	// }, 3000);
+	//
+	// setTimeout(function(){
+	// 		$( ".blinkingCircle" ).css( {"width": "2000px" } )
+	// 		$( ".blinkingCircle" ).css( {"height": "2000px" } )
+	// 	  $( ".blinkingCircle" ).css( {"transition": "40s"} )
+	// }, 3200);
+	// setTimeout(function(){
+	// 		$( ".blinkingCircle" ).css( {"opacity": "0" } )
+	// 		$( ".blinkingCircle" ).css( {"transition": "30s"} )
+	// }, 6300);
+	//
+	// setTimeout(function(){
+	// 		$( ".tracker" ).css( {"width": "110vw" } )
+	// }, 3100);
+	//
+	// setTimeout(function(){
+	// 		$( ".miniLogo" ).css( {"margin-right": "200px" } )
+	// 		$( ".miniLogo" ).css( {"letter-spacing": "0.4em" } )
+	// 		$( ".miniLogo" ).css( {"transition": "1s"} )
+	// }, 3700);
