36fa7a8e418111683947542c4515f4ec92faf855
Ekaterina Kozhara <ekaterina.kozhara@yandex.ru>
2019-03-17 21:35:49 +0300

    появление картинок

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-	// $(function(){
-	// 			$("#blinkingCircle").hide();
-	// 			setTimeout(function(){$("#blinkingCircle").show();setInterval(function(){$("#blinkingCircle").toggle();},400)},1000);
-	// 		});
-	//
-	// setTimeout(function(){
-	//     $( ".first" ).css( {"display": "none" } )
-	// 		$( ".first" ).css( {"transition": "3s"} )
-	// }, 3000);
-	//
-	// setTimeout(function(){
-	// 		$( ".background" ).css( {"background-color": "#FFE3E3" } )
-	// 	  $( ".background" ).css( {"transition": "3s"} )
-	// }, 3000);
-	//
-	// setTimeout(function(){
-	// 		$( ".blinkingCircle" ).css( {"width": "2000px" } )
-	// 		$( ".blinkingCircle" ).css( {"height": "2000px" } )
-	// 	  $( ".blinkingCircle" ).css( {"transition": "40s"} )
-	// }, 3200);
-	// setTimeout(function(){
-	// 		$( ".blinkingCircle" ).css( {"opacity": "0" } )
-	// 		$( ".blinkingCircle" ).css( {"transition": "30s"} )
-	// }, 6300);
-	//
-	// setTimeout(function(){
-	// 		$( ".tracker" ).css( {"width": "110vw" } )
-	// }, 3100);
-	//
-	// setTimeout(function(){
-	// 		$( ".miniLogo" ).css( {"margin-right": "200px" } )
-	// 		$( ".miniLogo" ).css( {"letter-spacing": "0.4em" } )
-	// 		$( ".miniLogo" ).css( {"transition": "1s"} )
-	// }, 3700);
+	$(function(){
+				$("#blinkingCircle").hide();
+				setTimeout(function(){$("#blinkingCircle").show();setInterval(function(){$("#blinkingCircle").toggle();},400)},1000);
+			});
+
+	setTimeout(function(){
+	    $( ".first" ).css( {"display": "none" } )
+			$( ".first" ).css( {"transition": "3s"} )
+	}, 4000);
+
+	setTimeout(function(){
+			$( ".background" ).css( {"background-color": "#FFE3E3" } )
+		  $( ".background" ).css( {"transition": "3s"} )
+	}, 4000);
+
+	setTimeout(function(){
+			$( ".blinkingCircle" ).css( {"width": "2000px" } )
+			$( ".blinkingCircle" ).css( {"height": "2000px" } )
+		  $( ".blinkingCircle" ).css( {"transition": "40s"} )
+	}, 4200);
+	setTimeout(function(){
+			$( ".blinkingCircle" ).css( {"opacity": "0" } )
+			$( ".blinkingCircle" ).css( {"transition": "30s"} )
+	}, 6000);
+
+	setTimeout(function(){
+			$( ".tracker" ).css( {"width": "110vw" } )
+	}, 4100);
+
+	setTimeout(function(){
+			$( ".miniLogo" ).css( {"margin-right": "200px" } )
+			$( ".miniLogo" ).css( {"letter-spacing": "0.4em" } )
+			$( ".miniLogo" ).css( {"transition": "1s"} )
+	}, 4700);
+
+	setTimeout(function(){
+			$( ".concert, .food, .piano, .send" ).css( {"opacity": "1" } )
+			$( ".concert, .food, .piano, .send" ).css( {"transition": "1s"} )
+	}, 6000);
